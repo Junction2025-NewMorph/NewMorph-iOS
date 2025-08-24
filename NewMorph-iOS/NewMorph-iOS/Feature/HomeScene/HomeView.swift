fd818405bfa8deb0ff502c58a3a4b3c99c666b6f
mini-min <mini@miniui-MacBookAir.local>
2025-08-24 10:19:29 +0900

    Jajiguri UI

--- NewMorph-iOS/NewMorph-iOS/Feature/HomeScene/HomeView.swift
+++ NewMorph-iOS/NewMorph-iOS/Feature/HomeScene/HomeView.swift
@@ -53,28 +53,31 @@ struct HomeView: View {
                         entry: entry,
                         onMicTapped: { withAnimation { isSheetPresented = true } }
                     )
+                    .padding(.horizontal, 20)
                     .transition(.asymmetric(
                         insertion: .move(edge: .leading).combined(with: .opacity),
                         removal: .move(edge: .trailing).combined(with: .opacity)
                     ))
+                    
+                    VStack {
+                        Spacer()
+                        if hasAnswer && subscene == .question {
+                            NMButton(action: { router.push(.result) }, title: "Done")
+                                .ignoresSafeArea(.keyboard, edges: .bottom)
+                        }
+                    }
                 }
 
                 if subscene == .calendar {
                     CalenderView()
-                    .transition(.asymmetric(
+                        .padding(.horizontal, 20)
+                        .transition(.asymmetric(
                         insertion: .move(edge: .trailing).combined(with: .opacity),
                         removal: .move(edge: .leading).combined(with: .opacity)
                     ))
                 }
             }
-            .padding(.horizontal, 20)
             .animation(.snappy, value: subscene)
-            
-            if hasAnswer && subscene == .question {
-                NMButton(action: { router.push(.result) }, title: "Done")
-                    .ignoresSafeArea(.keyboard, edges: .bottom)
-                    .zIndex(0)
-            }
         }
         .background(.nmBackground1Main)
         .onAppear {
